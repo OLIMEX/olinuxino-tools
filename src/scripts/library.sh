87dad108a8c3c1ef1647e6488eb95228a4460f55
Stefan Saraev <stefan@saraev.ca>
2020-05-28 17:01:40 +0300

    get_board_id(): temp i2c workaround

--- src/scripts/library.sh
+++ src/scripts/library.sh
@@ -26,10 +26,15 @@ function enable_overlays
 
 function get_board_id
 {
+    # TODO: cleanup this
+    i2c_adapter=1
+    if [ "$(cat /sys/class/i2c-adapter/i2c-0/name 2>/dev/null)" = "sun4i_hdmi_i2c adapter" ] ; then
+      i2c_adapter=2
+    fi
     local BOARD_ID=""
     for comp in $(cat "/proc/device-tree/compatible" | tr '\0' '\n'); do
 	    if [[ "$comp" == "olimex,"* ]]; then
-		    BOARD_ID=$((16#$(i2cget -f -y 2 0x50 0x04 w 2>/dev/null | sed 's/0x//g')))
+		    BOARD_ID=$((16#$(i2cget -f -y $i2c_adapter 0x50 0x04 w 2>/dev/null | sed 's/0x//g')))
 		    if [ "${BOARD_ID}" == "0" ] ; then
 			BOARD_ID=""
 		    fi
