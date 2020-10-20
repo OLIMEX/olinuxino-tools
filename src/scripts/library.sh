0a3195422d012fde1eea07f3defd99d570feb2ae
Stefan Saraev <stefan@saraev.ca>
2020-10-20 14:15:40 +0300

    a10 defaults

--- src/scripts/library.sh
+++ src/scripts/library.sh
@@ -11,6 +11,9 @@ function enable_overlays
 	if [ "${_soc}" = "sun50i-a64" ] ; then
 		_board_id="a64"
 	fi
+	if [ "${_soc}" = "sun4i-a10" ] ; then
+		_board_id="a10"
+	fi
 
 	[[ -z ${_board_id} ]] && return 0
 
